87bca928cafb1e9d6043e057e5a5e0dfd26a9202
sinyang <svenyang92@gmail.com>
2020-07-15 00:18:54 +1000

    add Premium Draw

--- src/main/java/com/sinyang/miraigbf/PluginMain.java
+++ src/main/java/com/sinyang/miraigbf/PluginMain.java
@@ -1,7 +1,5 @@
 package com.sinyang.miraigbf;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
 import net.mamoe.mirai.console.plugins.Config;
 import net.mamoe.mirai.console.plugins.ConfigSection;
 import net.mamoe.mirai.console.plugins.PluginBase;
@@ -11,23 +9,42 @@ import net.mamoe.mirai.message.MessageReceipt;
 import net.mamoe.mirai.message.data.*;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 class PluginMain extends PluginBase {
+    // Keywords
+    final private static String PRAISE_KEYWORD = "ø‰Œ“";
+    final private static String PREMIUM_DRAW_KEYWORD = " Æ¡¨";
 
+    // Auto Reply
     private Config autoReplyConfig;
     private ConfigSection autoReplyMap;
     final private String AUTOREPLY_CONFIG = "autoreply.yml";
     final private String AUTOREPLY = "autoreply";
 
+    // Premium draw pool
+    private Config premiumDrawPoolConfig;
+    private List<ConfigSection> poolItemsList;
+    private List<ConfigSection> rList = new ArrayList<>();
+    private List<ConfigSection> srList = new ArrayList<>();
+    private List<ConfigSection> ssrList = new ArrayList<>();
+    final private String PREMIUM_DRAW_POOL = "premium_draw_pool.yml";
+    final private String ITEMS = "items";
+
     public void onLoad() {
         super.onLoad();
+
+        // Load Auto Reply
         this.autoReplyConfig = this.loadConfig(AUTOREPLY_CONFIG);
         this.autoReplyMap = this.autoReplyConfig.getConfigSection(AUTOREPLY);
+
+        // Load premium draw pool
+        this.premiumDrawPoolConfig = this.loadConfig(PREMIUM_DRAW_POOL);
+        this.poolItemsList = this.premiumDrawPoolConfig.getConfigSectionList(ITEMS);
+        splitPoolItemList();
     }
 
     public void onEnable() {
@@ -81,7 +98,30 @@ class PluginMain extends PluginBase {
     }
 
     private void processGroupMessage(GroupMessage event, String message) {
-        Utils.processAutoReply(event, message, this.autoReplyMap);
-        Utils.processPraise(event, message);
+        if (MessageProcessor.processAutoReply(event, message, this.autoReplyMap)) {
+            getLogger().info("Processed auto reply. Skipping...");
+        } else if (message.contains(PRAISE_KEYWORD)) {
+            getScheduler().async(() -> {
+                MessageProcessor.processPraise(event);
+            });
+        } else if (message.equalsIgnoreCase(PREMIUM_DRAW_KEYWORD)) {
+            getScheduler().async(() -> {
+                MessageProcessor.processPremiumDraw(event, rList, srList, ssrList);
+            });
+        }
+    }
+
+    private void splitPoolItemList() {
+        for (int i = 0; i < poolItemsList.size(); i++) {
+            String rare = poolItemsList.get(i).getString("Rare");
+
+            if (rare.equalsIgnoreCase("R")) {
+                this.rList.add(poolItemsList.get(i));
+            } else if (rare.equalsIgnoreCase("SR")) {
+                this.srList.add(poolItemsList.get(i));
+            } else if (rare.equalsIgnoreCase("SSR")) {
+                this.ssrList.add(poolItemsList.get(i));
+            }
+        }
     }
 }          
